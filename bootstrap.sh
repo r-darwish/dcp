02c830d28b1488848221d56816054a432412c5b8
Roey Darwish Dror <roey.ghost@gmail.com>
2015-12-31 09:31:34 +0200

    Install emacs-nox

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,13 @@
-#!/bin/bash -e
+#!/bin/bash
+
+set -e
 
 echo "Installing dependencies"
 hash apt-get 2>/dev/null && sudo apt-get update
-hash apt-get 2>/dev/null && sudo apt-get install git tmux zsh vim python
+hash apt-get 2>/dev/null && sudo apt-get install git tmux zsh vim python emacs-nox
 hash yum 2>/dev/null && sudo yum install epel-release
-hash yum 2>/dev/null && sudo yum install git tmux zsh vim python
-hash pacman 2>/dev/null && sudo pacman -Sy --needed git tmux zsh vim python
+hash yum 2>/dev/null && sudo yum install git tmux zsh vim python emacs-nox
+hash pacman 2>/dev/null && sudo pacman -Sy --needed git tmux zsh vim python emacs-nox
 
 echo "Cloning DCP"
 git clone https://github.com/r-darwish/dcp.git ~/.dcp
