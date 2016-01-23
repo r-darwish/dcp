d469804c417b625b4f2359e47e92513f43b2b1d9
Roey Darwish Dror <roey.ghost@gmail.com>
2016-01-23 16:42:04 +0200

    Add flags for skipping VIM and Emacs

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,17 @@ set -e
 
 echo "Installing dependencies"
 hash apt-get 2>/dev/null && sudo apt-get update
-hash apt-get 2>/dev/null && sudo apt-get install git tmux zsh vim python emacs-nox
+hash apt-get 2>/dev/null && sudo apt-get install git tmux zsh python
 hash yum 2>/dev/null && sudo yum install epel-release
-hash yum 2>/dev/null && sudo yum install git tmux zsh vim python emacs-nox
-hash pacman 2>/dev/null && sudo pacman -Sy --needed git tmux zsh vim python emacs-nox
+hash yum 2>/dev/null && sudo yum install git tmux zsh python
+hash pacman 2>/dev/null && sudo pacman -Sy --needed git tmux zsh python
 
 echo "Cloning DCP"
 git clone https://github.com/r-darwish/dcp.git ~/.dcp
 
 echo "Installing DCP"
 cd ~/.dcp
-./install
+./install $@
 
 echo "Changing the default shell to ZSH"
 chsh -s $(which zsh)
